52117a466027f07342a272318f1859c2a70eaf2f
Tufu-Ma <surachai.ma@ku.th>
2025-02-07 16:50:46 +0700

    checkpoint

---
+++ Bankend/DB.js
@@ -0,0 +1,27 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'sa',
+    password: '@Farm2963789',//Your Password
+    server: 'TufuXD',//Your Server
+    database: 'SE',//Your Database
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+};
+
+async function checkConnection() {
+    try {
+        console.log("Connecting to database...");
+        const pool = await sql.connect(config);
+        console.log("Connection Successful!");
+        return pool;
+    } catch (err) {
+        console.error("Connection Failed:", err);
+        throw new Error("Database connection failed");
+    }
+}
+
+
+module.exports = checkConnection;
